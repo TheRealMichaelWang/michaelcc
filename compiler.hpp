41553abfe398925469375450a50a91840563719a
TheRealMichaelWang <mwang764@gatech.edu>
2026-01-08 08:16:41 -0800

    * Renamed linker.hpp and linker.cpp to compiler.hpp and compiler.cpp respectively

--- compiler.hpp
+++ compiler.hpp
@@ -3,5 +3,63 @@
 
 #include "ast.hpp"
 #include "logical.hpp"
+#include "typing.hpp"
 
+namespace michaelcc {
+    class compiler {
+    private:
+        logical_ir::translation_unit m_translation_unit;
+
+        compilation_error panic(const std::string& msg, const source_location& location) {
+            return compilation_error(msg, location);
+        }
+
+        class forward_declare_types final : public ast::visitor {
+        private:
+            compiler& m_linker;
+
+        public:
+            forward_declare_types(compiler& linker) : m_linker(linker) { }
+
+            void visit(const ast::struct_declaration& node) override;
+
+            void visit(const ast::union_declaration& node) override;
+
+            void visit(const ast::enum_declaration& node) override;
+        };
+
+        class implement_type_declarations final : public ast::visitor {
+        private:
+            compiler& m_linker;
+
+        public:
+            implement_type_declarations(compiler& linker) : m_linker(linker) { }
+
+            void visit(const ast::struct_declaration& node) override;
+
+            void visit(const ast::union_declaration& node) override;
+        };
+
+        class forward_declare_functions final : public ast::visitor {
+        private:
+            compiler& m_linker;
+
+            void forward_declare_function(const std::string& function_name, const std::vector<ast::function_parameter>& parameters, const source_location& location);
+        public:
+            forward_declare_functions(compiler& linker) : m_linker(linker) { }
+
+            void visit(const ast::function_declaration& node) override {
+                forward_declare_function(node.function_name(), node.parameters(), node.location());
+            }
+
+            void visit(const ast::function_prototype& node) override {
+                forward_declare_function(node.function_name(), node.parameters(), node.location());
+            }
+        };
+
+        std::unique_ptr<typing::type> resolve_type(const ast::ast_element& type);
+    public:
+        compiler() : m_translation_unit() { }
+    };
+}
 #endif
