e5ff1c7970256e0c0ea878d899f3fc3c18cef6bc
TheRealMichaelWang <mwang764@gatech.edu>
2026-01-08 19:41:01 -0800

    * Added support for generic dispatcher * Added support for detecting memory layout dependencies in type declarations

--- compiler.cpp
+++ compiler.cpp
@@ -1,7 +1,58 @@
 #include "compiler.hpp"
+#include "typing.hpp"
+#include <memory>
+#include <queue>
+#include <sstream>
 
 using namespace michaelcc;
 
-void compiler::calculate_type_sizes(std::shared_ptr<typing::type>& type) {
+void compiler::resolve_layout_dependencies(std::shared_ptr<typing::type>& type) const {
+    layout_dependency_getter resolver(m_translation_unit);
+    
+    std::map<std::shared_ptr<typing::type>, std::shared_ptr<typing::type>> last_seen_parent;
 
+    std::queue<std::shared_ptr<typing::type>> type_to_resolve;
+    type_to_resolve.push(type);
+
+    while (!type_to_resolve.empty()) {
+        std::shared_ptr<typing::type> type = type_to_resolve.front();
+        if (last_seen_parent.contains(type)) { //check for circular dependencies
+            std::ostringstream ss;
+
+            ss << "Circular memory layout dependency detected with type " << type->to_string();
+            if (m_type_declaration_locations.contains(type)) {
+                ss << "(at " << m_type_declaration_locations.at(type).to_string() << ')';
+            }
+
+            std::vector<std::shared_ptr<typing::type>> path;
+            std::shared_ptr<typing::type> current = last_seen_parent[type];
+            while (current != type) {
+                path.push_back(current);
+                current = last_seen_parent[current];
+            }
+            path.push_back(type);
+            std::reverse(path.begin(), path.end());
+            ss << " in the following path: ";
+
+            for (const auto& path_type : path) {
+                ss << path_type->to_string();
+                if (m_type_declaration_locations.contains(path_type)) {
+                    ss << " (at " << m_type_declaration_locations.at(path_type).to_string() << ')';
+                }
+                if (path_type != type) {
+                    ss << " -> ";
+                }
+            }
+            ss << '.';
+
+            throw compilation_error(ss.str(), m_type_declaration_locations.at(type));
+        }
+        type_to_resolve.pop();
+
+        const std::vector<std::shared_ptr<typing::type>> dependencies = resolver.dispatch_dynamic(*type);
+        for (const auto& dependency : dependencies) {
+            type_to_resolve.push(dependency);
+            last_seen_parent[dependency] = type;
+        }
+    }
 }
--- utils.hpp
+++ utils.hpp
@@ -1,6 +1,8 @@
 #ifndef MICHAELCC_UTILS_HPP
 #define MICHAELCC_UTILS_HPP
 
+#include <type_traits>
+
 namespace michaelcc {
     template<typename... NodeTypes>
     class generic_visitor;
@@ -24,6 +26,42 @@ namespace michaelcc {
     public:
         virtual ~visitable_base() = default;
         virtual void accept(Visitor& v) const = 0;
+    };
+
+    template<typename ReturnType, typename... NodeTypes>
+    class generic_dispatcher;
+
+    template<typename ReturnType>
+    class generic_dispatcher<ReturnType> {
+    public:
+        virtual ~generic_dispatcher() = default;
+
+        template<typename BaseType>
+        ReturnType dispatch_dynamic(const BaseType&) const {
+            return ReturnType{};
+        }
+
+    protected:
+        // Sentinel for the using declaration chain - never called
+        void dispatch() const {}
+    };
+
+    template<typename ReturnType, typename NodeType, typename... Rest>
+    class generic_dispatcher<ReturnType, NodeType, Rest...> : public generic_dispatcher<ReturnType, Rest...> {
+    protected:
+        using generic_dispatcher<ReturnType, Rest...>::dispatch;
+        virtual ReturnType dispatch(const NodeType& node) const = 0;
+
+    public:
+        template<typename BaseType>
+        ReturnType dispatch_dynamic(const BaseType& node) const {
+            static_assert(std::is_base_of_v<BaseType, NodeType>, 
+                "BaseType must be a base class of NodeType");
+            if (auto* p = dynamic_cast<const NodeType*>(&node)) {
+                return dispatch(*p);
+            }
+            return generic_dispatcher<ReturnType, Rest...>::dispatch_dynamic(node);
+        }
     };
 }
 
