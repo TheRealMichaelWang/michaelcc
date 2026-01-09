955dad14af1f362c8051f9f4ad92e73f5970ebc1
TheRealMichaelWang <mwang764@gatech.edu>
2026-01-09 15:48:14 -0800

    * Added support for qualified types

--- logical.hpp
+++ logical.hpp
@@ -70,13 +70,13 @@ namespace michaelcc {
 		class variable final : public symbol, public visitable_base<visitor> {
 		private:
             uint8_t m_qualifiers;
-            std::shared_ptr<typing::type> m_type;
+            typing::qual_type m_type;
 			bool m_is_global;
 		public:
-			variable(std::string&& name, std::shared_ptr<typing::type>&& var_type, bool is_global, std::weak_ptr<symbol_context>&& context)
+			variable(std::string&& name, typing::qual_type&& var_type, bool is_global, std::weak_ptr<symbol_context>&& context)
 				: symbol(std::move(name), std::move(context)), m_type(std::move(var_type)), m_is_global(is_global) {}
 
-			const std::shared_ptr<typing::type> get_type() const noexcept { return m_type; }
+			const typing::qual_type& get_type() const noexcept { return m_type; }
 			bool is_global() const noexcept { return m_is_global; }
 
             std::string to_string() const noexcept override {
@@ -89,21 +89,21 @@ namespace michaelcc {
 		class expression : public visitable_base<visitor> {
 		public:
 			virtual ~expression() = default;
-			virtual const std::shared_ptr<typing::type> get_type() const = 0;
+			virtual const typing::qual_type get_type() const = 0;
 			virtual void accept(visitor& v) const override = 0;
 		};
 
 		class integer_constant final : public expression {
 		private:
 			uint64_t m_value;
-			std::shared_ptr<typing::int_type> m_type;
+			typing::qual_type m_type;
 		public:
-			integer_constant(uint64_t value, std::shared_ptr<typing::int_type>&& int_type)
+			integer_constant(uint64_t value, typing::qual_type&& int_type)
 				: m_value(value), m_type(std::move(int_type)) {}
 
 			uint64_t value() const noexcept { return m_value; }
 			
-            const std::shared_ptr<typing::type> get_type() const override { 
+            const typing::qual_type get_type() const override { 
                 return m_type; 
             }
 
@@ -113,14 +113,14 @@ namespace michaelcc {
 		class floating_constant final : public expression {
 		private:
 			double m_value;
-			std::shared_ptr<typing::float_type> m_type;
+			typing::qual_type m_type;
 		public:
-			floating_constant(double value, std::shared_ptr<typing::float_type>&& float_type)
+			floating_constant(double value, typing::qual_type&& float_type)
 				: m_value(value), m_type(std::move(float_type)) {}
 
 			double value() const noexcept { return m_value; }
 
-			const std::shared_ptr<typing::type> get_type() const override { 
+			const typing::qual_type get_type() const override { 
                 return m_type; 
             }
 
@@ -134,8 +134,10 @@ namespace michaelcc {
 			explicit string_constant(size_t index) : m_index(index) {}
 
 			size_t index() const noexcept { return m_index; }
-			const std::shared_ptr<typing::type> get_type() const override { 
-                return std::make_shared<typing::pointer_type>(std::make_shared<typing::int_type>(0, typing::CHAR_INT_CLASS)); 
+			const typing::qual_type get_type() const override { 
+                return typing::qual_type(std::make_shared<typing::pointer_type>(
+                    typing::weak_qual_type( typing::primitives::char_t)
+                ));     
             }
 
 			void accept(visitor& v) const override { v.visit(*this); }
@@ -150,7 +152,7 @@ namespace michaelcc {
 			const std::shared_ptr<variable>& get_variable() const noexcept { return m_variable; }
 			bool is_global() const noexcept { return m_variable->is_global(); }
 
-			const std::shared_ptr<typing::type> get_type() const override {
+			const typing::qual_type get_type() const override {
 				return m_variable->get_type();
 			}
 
@@ -163,12 +165,12 @@ namespace michaelcc {
 			std::unique_ptr<expression> m_left;
 			std::unique_ptr<expression> m_right;
 
-            std::shared_ptr<typing::type> m_result_type;
+            typing::qual_type m_result_type;
 		public:
 			binary_operation(token_type op,
 				std::unique_ptr<expression>&& left,
 				std::unique_ptr<expression>&& right,
-				std::shared_ptr<typing::type>&& result_type)
+				typing::qual_type&& result_type)
 				: m_operator(op),
 				m_left(std::move(left)),
 				m_right(std::move(right)),
@@ -178,7 +180,7 @@ namespace michaelcc {
 			const expression* left() const noexcept { return m_left.get(); }
 			const expression* right() const noexcept { return m_right.get(); }
 			
-            const std::shared_ptr<typing::type> get_type() const override { return m_result_type; }
+            const typing::qual_type get_type() const override { return m_result_type; }
 
 			void accept(visitor& v) const override {
 				v.visit(*this);
@@ -200,7 +202,7 @@ namespace michaelcc {
 			token_type get_operator() const noexcept { return m_operator; }
             const expression* operand() const noexcept { return m_operand.get(); }
 
-            const std::shared_ptr<typing::type> get_type() const override { return m_operand->get_type(); }
+            const typing::qual_type get_type() const override { return m_operand->get_type(); }
 
 			void accept(visitor& v) const override {
 				v.visit(*this);
@@ -211,13 +213,13 @@ namespace michaelcc {
 		class type_cast final : public expression {
 		private:
 			std::unique_ptr<expression> m_operand;
-			std::shared_ptr<typing::type> m_target_type;
+			typing::qual_type m_target_type;
 		public:
-			type_cast(std::unique_ptr<expression>&& operand, std::shared_ptr<typing::type>&& target_type)
+			type_cast(std::unique_ptr<expression>&& operand, typing::qual_type&& target_type)
 				: m_operand(std::move(operand)), m_target_type(std::move(target_type)) {}
 
 			const expression* operand() const noexcept { return m_operand.get(); }
-			const std::shared_ptr<typing::type> get_type() const override { return m_target_type; }
+			const typing::qual_type get_type() const override { return m_target_type; }
 
 			void accept(visitor& v) const override {
 				v.visit(*this);
@@ -234,8 +236,8 @@ namespace michaelcc {
 
 			const expression* operand() const noexcept { return m_operand.get(); }
 
-			const std::shared_ptr<typing::type> get_type() const override { 
-                return std::make_shared<typing::pointer_type>(m_operand->get_type());
+			const typing::qual_type get_type() const override { 
+                return typing::qual_type(std::make_shared<typing::pointer_type>(m_operand->get_type().to_weak()));
             }
 
 			void accept(visitor& v) const override {
@@ -252,10 +254,13 @@ namespace michaelcc {
 
 			const expression* operand() const noexcept { return m_operand.get(); }
 
-			const std::shared_ptr<typing::type> get_type() const override {
+			const typing::qual_type get_type() const override {
                 auto operand_type = m_operand->get_type();
-				auto* ptr_type = dynamic_cast<const typing::pointer_type*>(operand_type.get());
-				return ptr_type ? ptr_type->pointee_type() : nullptr;
+				auto* ptr_type = dynamic_cast<const typing::pointer_type*>(operand_type.type().get());
+                if (ptr_type == nullptr) {
+                    throw std::runtime_error("Operand is not a pointer");
+                }
+				return ptr_type->pointee_type().to_shared();
 			}
 
 			void accept(visitor& v) const override {
@@ -268,14 +273,14 @@ namespace michaelcc {
 		private:
 			std::unique_ptr<expression> m_base;
 			size_t m_field_index;
-			std::shared_ptr<typing::type> m_field_type; 
+			typing::qual_type m_field_type; 
 		public:
-			member_access(std::unique_ptr<expression>&& base, size_t field_index, std::shared_ptr<typing::type>&& field_type)
+			member_access(std::unique_ptr<expression>&& base, size_t field_index, typing::qual_type&& field_type)
 				: m_base(std::move(base)), m_field_index(field_index), m_field_type(std::move(field_type)) {}
 
 			const expression* base() const noexcept { return m_base.get(); }
 			size_t field_index() const noexcept { return m_field_index; }
-			const std::shared_ptr<typing::type> get_type() const override { return m_field_type; }
+			const typing::qual_type get_type() const override { return m_field_type; }
 
 			void accept(visitor& v) const override {
 				v.visit(*this);
@@ -294,15 +299,14 @@ namespace michaelcc {
 			const expression* base() const noexcept { return m_base.get(); }
 			const expression* index() const noexcept { return m_index.get(); }
 
-			const std::shared_ptr<typing::type> get_type() const override {
+			const typing::qual_type get_type() const override {
 				auto base_type = m_base->get_type();
-				auto* arr_type = dynamic_cast<const typing::array_type*>(base_type.get());
-				if (arr_type != nullptr) {
-					return arr_type->element_type();
-				}
-				auto* ptr_type = dynamic_cast<const typing::pointer_type*>(base_type.get());
-				return ptr_type ? std::make_shared<typing::pointer_type>(ptr_type->pointee_type()) : nullptr;
-			}
+				auto* arr_type = dynamic_cast<const typing::array_type*>(base_type.type().get());
+				if (arr_type == nullptr) {
+                    throw std::runtime_error("Base is not an array");
+                }
+				return arr_type->element_type().to_shared();
+            }
 
 			void accept(visitor& v) const override {
 				v.visit(*this);
@@ -323,14 +327,13 @@ namespace michaelcc {
 			const expression* callee() const noexcept { return m_callee.get(); }
 			const std::vector<std::unique_ptr<expression>>& arguments() const noexcept { return m_arguments; }
 
-			const std::shared_ptr<typing::type> get_type() const override {
+			const typing::qual_type get_type() const override {
 				auto callee_type = m_callee->get_type();
-				auto* ptr_type = dynamic_cast<const typing::pointer_type*>(callee_type.get());
-				if (ptr_type != nullptr) {
-					return ptr_type->pointee_type();
-				}
-				auto* fn_type = dynamic_cast<const typing::function_pointer_type*>(callee_type.get());
-				return fn_type ? fn_type->return_type() : nullptr;
+				auto* fn_type = dynamic_cast<const typing::function_pointer_type*>(callee_type.type().get());
+				if (fn_type == nullptr) {
+                    throw std::runtime_error("Callee is not a function pointer");
+                }
+				return fn_type->return_type().to_shared();
 			}
 
 			void accept(visitor& v) const override {
@@ -347,12 +350,12 @@ namespace michaelcc {
 			std::unique_ptr<expression> m_condition;
 			std::unique_ptr<expression> m_then_expression;
 			std::unique_ptr<expression> m_else_expression;
-			std::shared_ptr<typing::type> m_result_type;
+			typing::qual_type m_result_type;
 		public:
 			conditional_expression(std::unique_ptr<expression>&& condition,
 				std::unique_ptr<expression>&& then_expression,
 				std::unique_ptr<expression>&& else_expression,
-				std::shared_ptr<typing::type>&& result_type)
+				typing::qual_type&& result_type)
 				: m_condition(std::move(condition)),
 				m_then_expression(std::move(then_expression)),
 				m_else_expression(std::move(else_expression)),
@@ -362,7 +365,7 @@ namespace michaelcc {
 			const expression* then_expression() const noexcept { return m_then_expression.get(); }
 			const expression* else_expression() const noexcept { return m_else_expression.get(); }
 			
-            const std::shared_ptr<typing::type> get_type() const override { 
+            const typing::qual_type get_type() const override { 
                 return m_result_type; 
             }
 
