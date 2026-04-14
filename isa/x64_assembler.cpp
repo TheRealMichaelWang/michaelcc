8a5f247309d973e7fb5e62c77157635cc288ff61
Michael Wang <mwang764@gatech.edu>
2026-04-14 10:20:33 -0400

    * Added support for float arithmetic ops

--- isa/x64_assembler.cpp
+++ isa/x64_assembler.cpp
@@ -8,6 +8,26 @@ void x64_assembler::emit_unsigned_multiply(linear::virtual_register dest, linear
     throw std::runtime_error("Unsigned multiply is not supported yet for x64");
 }
 
+void x64_assembler::emit_signed_multiply(linear::virtual_register dest, linear::virtual_register operand_a, linear::virtual_register operand_b) {
+    throw std::runtime_error("Signed multiply is not supported yet for x64");
+}
+
+void x64_assembler::emit_signed_divide(linear::virtual_register dest, linear::virtual_register operand_a, linear::virtual_register operand_b) {
+    throw std::runtime_error("Signed divide is not supported yet for x64");
+}
+
+void x64_assembler::emit_signed_remainder(linear::virtual_register dest, linear::virtual_register operand_a, linear::virtual_register operand_b) {
+    throw std::runtime_error("Signed remainder is not supported yet for x64");
+}
+
+void x64_assembler::emit_unsigned_divide(linear::virtual_register dest, linear::virtual_register operand_a, linear::virtual_register operand_b) {
+    throw std::runtime_error("Unsigned divide is not supported yet for x64");
+}
+
+void x64_assembler::emit_unsigned_remainder(linear::virtual_register dest, linear::virtual_register operand_a, linear::virtual_register operand_b) {
+    throw std::runtime_error("Unsigned remainder is not supported yet for x64");
+}
+
 void x64_assembler::dispatch(const linear::a_instruction& instruction) {
     switch (instruction.type()) {
     case linear::MICHAELCC_LINEAR_A_ADD:
@@ -43,9 +63,6 @@ void x64_assembler::dispatch(const linear::a_instruction& instruction) {
     auto physical_reg_a = get_physical_register(instruction.operand_a());
     auto physical_reg_b = get_physical_register(instruction.operand_b());
 
-    assert(instruction.operand_a().reg_size == instruction.operand_b().reg_size);
-    assert(instruction.destination_register()->reg_size == instruction.operand_a().reg_size);
-
     begin_new_line();
     switch (instruction.type()) {
     case linear::MICHAELCC_LINEAR_A_ADD:
@@ -63,6 +80,128 @@ void x64_assembler::dispatch(const linear::a_instruction& instruction) {
             begin_new_line();
         }
         m_output << "sub " << physical_reg_dest.name << ", " << physical_reg_b.name;
+        break;
+
+    // float arithmetic
+    case linear::MICHAELCC_LINEAR_A_FLOAT_ADD:
+        if (instruction.destination().reg_size == linear::word_size::MICHAELCC_WORD_SIZE_UINT64) {
+            if (instruction.operand_a() == instruction.destination()) { m_output << "addsd " << physical_reg_dest.name << ", " << physical_reg_b.name; }
+            else if (instruction.operand_b() == instruction.destination()) { m_output << "addsd " << physical_reg_dest.name << ", " << physical_reg_a.name; }
+            else { 
+                m_output << "movsd " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+                m_output << "addsd " << physical_reg_dest.name << ", " << physical_reg_b.name;
+            }
+        } else {
+            if (instruction.operand_a() == instruction.destination()) { m_output << "addss " << physical_reg_dest.name << ", " << physical_reg_b.name; }
+            else if (instruction.operand_b() == instruction.destination()) { m_output << "addss " << physical_reg_dest.name << ", " << physical_reg_a.name; }
+            else { 
+                m_output << "movss " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+                m_output << "addss " << physical_reg_dest.name << ", " << physical_reg_b.name;
+            }
+        }
+        break;
+    case linear::MICHAELCC_LINEAR_A_FLOAT_SUBTRACT:
+        if (instruction.destination().reg_size == linear::word_size::MICHAELCC_WORD_SIZE_UINT64) {
+            if (instruction.operand_a() != instruction.destination()) {
+                m_output << "movsd " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+            }
+            m_output << "subsd " << physical_reg_dest.name << ", " << physical_reg_b.name;
+        } else {
+            if (instruction.operand_a() != instruction.destination()) {
+                m_output << "movss " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+            }
+            m_output << "subss " << physical_reg_dest.name << ", " << physical_reg_b.name;
+        }
+        break;
+    case linear::MICHAELCC_LINEAR_A_FLOAT_MULTIPLY:
+        if (instruction.destination().reg_size == linear::word_size::MICHAELCC_WORD_SIZE_UINT64) {
+            if (instruction.operand_a() == instruction.destination()) { m_output << "mulsd " << physical_reg_dest.name << ", " << physical_reg_b.name; }
+            else if (instruction.operand_b() == instruction.destination()) { m_output << "mulsd " << physical_reg_dest.name << ", " << physical_reg_a.name; }
+            else { 
+                m_output << "movsd " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+                m_output << "mulsd " << physical_reg_dest.name << ", " << physical_reg_b.name;
+            }
+        } else {
+            if (instruction.operand_a() == instruction.destination()) { m_output << "mulss " << physical_reg_dest.name << ", " << physical_reg_b.name; }
+            else if (instruction.operand_b() == instruction.destination()) { m_output << "mulss " << physical_reg_dest.name << ", " << physical_reg_a.name; }
+            else { 
+                m_output << "movss " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+                m_output << "mulss " << physical_reg_dest.name << ", " << physical_reg_b.name;
+            }
+        }
+        break;
+    case linear::MICHAELCC_LINEAR_A_FLOAT_DIVIDE:
+        if (instruction.destination().reg_size == linear::word_size::MICHAELCC_WORD_SIZE_UINT64) {
+            if (instruction.operand_a() == instruction.destination()) { m_output << "divsd " << physical_reg_dest.name << ", " << physical_reg_b.name; }
+            else if (instruction.operand_b() == instruction.destination()) { m_output << "divsd " << physical_reg_dest.name << ", " << physical_reg_a.name; }
+            else { 
+                m_output << "movsd " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+                m_output << "divsd " << physical_reg_dest.name << ", " << physical_reg_b.name;
+            }
+        } else {
+            if (instruction.operand_a() == instruction.destination()) { m_output << "divss " << physical_reg_dest.name << ", " << physical_reg_b.name; }
+            else if (instruction.operand_b() == instruction.destination()) { m_output << "divss " << physical_reg_dest.name << ", " << physical_reg_a.name; }
+            else { 
+                m_output << "movss " << physical_reg_dest.name << ", " << physical_reg_a.name;
+                begin_new_line();
+                m_output << "divss " << physical_reg_dest.name << ", " << physical_reg_b.name;
+            }
+        }
+        break;
+    case linear::MICHAELCC_LINEAR_A_FLOAT_MODULO:
+        throw std::runtime_error("Float modulo is not supported yet for x64. Just use an fmod or subtract quotient manually.");
+    
+    default: throw std::runtime_error("Invalid arithmetic/logical/comparison instruction type");
+    }
+}
+
+void x64_assembler::dispatch(const linear::a2_instruction& instruction) {
+    auto physical_reg_dest = get_physical_register(instruction.destination());
+    auto physical_reg_a = get_physical_register(instruction.operand_a());
+    
+    begin_new_line();
+    switch (instruction.type()) {
+    case linear::MICHAELCC_LINEAR_A_ADD:
+        if (instruction.destination() != instruction.operand_a()) {
+            m_output << "mov " << physical_reg_dest.name << ", " << physical_reg_a.name;
+            begin_new_line();
+        }
+        m_output << "add " << physical_reg_dest.name << ", " << instruction.constant();
+        break;
+    case linear::MICHAELCC_LINEAR_A_SUBTRACT:
+        if (instruction.destination() != instruction.operand_a()) {
+            m_output << "mov " << physical_reg_dest.name << ", " << physical_reg_a.name;
+            begin_new_line();
+        }
+        m_output << "sub " << physical_reg_dest.name << ", " << instruction.constant();
+        break;
+    case linear::MICHAELCC_LINEAR_A_SHIFT_LEFT:
+        if (instruction.destination() != instruction.operand_a()) {
+            m_output << "mov " << physical_reg_dest.name << ", " << physical_reg_a.name;
+            begin_new_line();
+        }
+        m_output << "shl " << physical_reg_dest.name << ", " << instruction.constant();
+        break;
+    case linear::MICHAELCC_LINEAR_A_SIGNED_SHIFT_RIGHT:
+        if (instruction.destination() != instruction.operand_a()) {
+            m_output << "mov " << physical_reg_dest.name << ", " << physical_reg_a.name;
+            begin_new_line();
+        }
+        m_output << "sar " << physical_reg_dest.name << ", " << instruction.constant();
+        break;
+    case linear::MICHAELCC_LINEAR_A_UNSIGNED_SHIFT_RIGHT:
+        if (instruction.destination() != instruction.operand_a()) {
+            m_output << "mov " << physical_reg_dest.name << ", " << physical_reg_a.name;
+            begin_new_line();
+        }
+        m_output << "shr " << physical_reg_dest.name << ", " << instruction.constant();
         break;
     default: throw std::runtime_error("Invalid arithmetic/logical/comparison instruction type");
     }
